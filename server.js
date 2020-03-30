ce8e461e453f7629ca77c7ecb7ff8b79597de289
Glitch (fire-royal-birth) <none>
2020-03-30 06:17:42 +0000

    🚖🕵 Checkpoint ./server.js:12068889/1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = ''
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` BY|🔸ARBELE🔸`, "http://twitch.tv/S-F")
+client.user.setGame(` BY|🔸ARBELE🔸|`, "http://twitch.tv/S-F")
  
 });
 
