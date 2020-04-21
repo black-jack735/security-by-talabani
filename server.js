ca1fb20241f27692ce62b2e8ed501353831a2116
Glitch (fire-royal-birth) <none>
2020-04-21 16:45:40 +0000

    🐑🛩 Checkpoint ./server.js:12068889/5782

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ client.on('guildMemberAdd', member => {
 
      
 
-client.login('NjkyOTcwMTk2MzgxNzI4Nzg5.Xp8Gzw.JOr4LnSWi6SD7Fh9PbrPgLy5h2Y');
+client.login('');
         
 
 
@@ -78,7 +78,7 @@ client.on('guildMemberRemove', member => {
  
  ///////////////////  
  
-client.login('NjkyOTcwMTk2MzgxNzI4Nzg5.Xp8GaQ.5CDeGDENrrO17PM91U60XH5-0AI');
+client.login('');
 
 
 
